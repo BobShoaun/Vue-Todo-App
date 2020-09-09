f33bbcb2dfb792317b70d511fb92cdf7bbb5ddee
Ng Bob Shoaun <ngbobshoaun2000@gmail.com>
2020-09-10 02:01:24 +0800

    small tweak

--- src/components/AddTodo.vue
+++ src/components/AddTodo.vue
@@ -1,7 +1,7 @@
 <template>
   <form @submit.prevent="add" class="grid grid-cols-12 gap-3">
     <input class="col-span-7 rounded-sm text-gray-800 border shadow h-6 px-2" type="text" name="title" placeholder="todo title..." v-model="inputTitle" />
-    <input class="col-span-3 rounded-sm text-gray-800 border shadow h-6 px-2" type="number" name="title" placeholder="priority" v-model="priority" />
+    <input class="col-span-3 rounded-sm text-gray-800 border shadow h-6 px-2" type="number" name="title" placeholder="priority..." v-model="priority" />
     <input class="col-span-2 rounded-sm text-gray-800 font-bold h-6 border bg-yellow-300 hover:bg-yellow-400" type="submit" value="add" />
   </form>
   
