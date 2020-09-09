8feaf7c75a50c8126bec8ab88d9389c00dcfc56f
Ng Bob Shoaun <ngbobshoaun2000@gmail.com>
2020-09-10 01:58:50 +0800

    made ui instant

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,13 @@
 <template>
   <div id="app" class="max-w-lg m-auto my-10">
 
-    <h1 class="text-gray-800 text-center font-bold text-5xl">things to do</h1>
+    <h1 class="text-gray-800 text-center font-bold text-5xl">things to do:</h1>
     <div class="my-3 p-8 pb-6 bg-gray-800 rounded shadow-lg">
       <TodoList v-bind:todos="incompletedTodos" 
                 v-on:delete-todo="deleteTodo"
                 v-on:update-todo="updateTodo"
                 class="mb-3" />
-      <AddTodo v-on:add-todo="addTodo" class="my-4 mb-8"/>
+      <AddTodo v-on:add-todo="addTodo" class="mt-4 mb-8"/>
       <hr>
       <h4 class="my-2 text-gray-400 italic">marked as complete</h4>
       <TodoList v-bind:todos="completedTodos" 
@@ -35,13 +35,27 @@ export default {
   },
   
   methods: {
+
     async deleteTodo(id) {
-      await db.collection('todos').doc(id).delete();
       this.todos = this.todos.filter(todo => todo.id !== id);
       this.separateTodos();
+      await db.collection('todos').doc(id).delete();
     },
 
     async addTodo(title, priority) {
+      const newTodo_client = {
+        id: -1,
+        title,
+        completed: false,
+        priority,
+        dateCreated: new Date(),
+        editingTitle: false,
+        editingPriority: false,
+        allowModify: false,
+      }
+      this.todos.push(newTodo_client); // client only
+      this.separateTodos();
+
       const newTodo = {
         title,
         completed: false,
@@ -50,11 +64,14 @@ export default {
       };
       const docRef = await db.collection('todos').add(newTodo);
       const doc = await docRef.get();
+      this.todos = this.todos.filter(todo => todo.id !== -1);
       this.todos.push(this.createTodo(doc));
       this.separateTodos();
     },
 
     updateTodo(todo) {
+      if (!todo.allowModify)
+        return;
       const updatedTodo = {
         title: todo.title,
         completed: todo.completed,
@@ -66,9 +83,13 @@ export default {
     },
 
     async getTodos() {
-      const querySnapshot = await db.collection("todos").get();
-      querySnapshot.forEach(doc => this.todos.push(this.createTodo(doc)));
-      this.separateTodos();
+      try {
+        const querySnapshot = await db.collection("todos").get();
+        querySnapshot.forEach(doc => this.todos.push(this.createTodo(doc)));
+        this.separateTodos();
+      } catch {
+        alert("Problem loading todos!");
+      }
     },
 
     separateTodos() {
@@ -91,7 +112,8 @@ export default {
         priority: doc.data().priority,
         dateCreated: doc.data().dateCreated,
         editingTitle: false,
-        editingPriority: false
+        editingPriority: false,
+        allowModify: true
       }
     }
   },
