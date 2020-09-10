d84a3fb6d17a6b62b290df3e8a7843c153c1d838
Ng Bob Shoaun <ngbobshoaun2000@gmail.com>
2020-09-10 15:55:53 +0800

    tweaks

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Home from '@/views/Home.vue'
+import About from '@/views/About.vue'
 
 Vue.use(VueRouter)
 
@@ -11,14 +12,14 @@ Vue.use(VueRouter)
     component: Home
   },
   {
+    path: '/about',
+    name: 'About',
+    component: About
+  },
+  {
     path: '*',
     name: 'Error 404',
     component: () => import('../views/404.vue')
-  },
-  {
-    path: '/about',
-    name: 'About',
-    component: () => import('../views/About.vue')
   },
   {
     path: '/secret',
